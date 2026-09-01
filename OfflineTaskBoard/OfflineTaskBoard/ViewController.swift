1ca50247c3c5a0c0f15bd61060952336d6c1b822
Gargi Garg <garggargi1105@gmail.com>
2026-09-01 23:00:43 +0530

    Initial iOS project setup

---
+++ OfflineTaskBoard/OfflineTaskBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OfflineTaskBoard
+//
+//  Created by Aditya Sinha on 01/09/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
